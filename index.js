8ce5d2a3f948a2052b0e0a1d5e378930dee70ded
Curt Bryan <kurdtbryan@gmail.com>
2019-04-29 12:05:19 -0700

    doneByMe

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("Hello World");
+const element = document.getElementById("counter");
+let count = 0;
+
+function increase() {
+  count += 1;
+  console.dir(element);
+  element.innerText = count;
+}
+
+function decrease() {
+  count -= 1;
+  console.log(count);
+  element.innerText = count;
+}
+// lecture way
+// function reset() {
+//   count = 0;
+//   console.log(count);
+//   element.innerText = count;
+// }
+//mini solution way
+function reset() {
+  count = 0;
+  console.log(count);
+  element.innerHTML = "<mark>" + count + "</mark>";
+}
+
+function selectTheme(theme) {
+  document.getElementsByTagName("body")[0].className = theme;
+  document.getElementsByTagName("main")[0].className = theme;
+  const buttons = document.getElementsByTagName("button");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].className = theme;
+  }
+}
